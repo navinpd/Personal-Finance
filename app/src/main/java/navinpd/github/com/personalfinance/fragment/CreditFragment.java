f7682f5908b24965882a9e744c2a833fee67adde
Navin prasad <Navin@Navins-MacBook-Pro.local>
2016-06-21 00:04:27 +0800

    First commit

---
+++ app/src/main/java/navinpd/github/com/personalfinance/fragment/CreditFragment.java
@@ -0,0 +1,26 @@
+package navinpd.github.com.personalfinance.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import navinpd.github.com.personalfinance.R;
+
+/**
+ * Created by Navin on 20/06/16.
+ */
+public class CreditFragment extends Fragment {
+    public CreditFragment () {
+
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.input_fragment, container, false);
+    }
+}
